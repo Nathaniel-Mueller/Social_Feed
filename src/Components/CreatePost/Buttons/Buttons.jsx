c43598df7e7333622a1c0e6ac6b0e88f98aa83d1
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-11-21 19:54:42 -0800

    Added empty string alt for all button images

--- src/Components/CreatePost/Buttons/Buttons.jsx
+++ src/Components/CreatePost/Buttons/Buttons.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Buttons.css'
 
 
@@ -7,37 +7,37 @@ const Buttons = (props) => {
 
     function handleLikeClick(){
         if (dislikeButtonActive === 'active'){
-            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')} />)
+            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')} alt = ''/>)
             setDislikeButton('inactive')
         }
         if (likeButtonActive === 'inactive'){
             setLikeButton('active')
-            setLike(<img src={require('../../.././assets/like-button-active.png')}/>)
+            setLike(<img src={require('../../.././assets/like-button-active.png')} alt = ''/>)
         }
         else {
             setLikeButton('inactive')
-            setLike(<img src={require('../../.././assets/like-button-inactive.png')}/>)
+            setLike(<img src={require('../../.././assets/like-button-inactive.png')} alt = ''/>)
         }
     }
 
     function handleDislikeClick(){
         if (likeButtonActive === 'active'){
-            setLike(<img src={require('../../.././assets/like-button-inactive.png')} />)
+            setLike(<img src={require('../../.././assets/like-button-inactive.png')} alt = ''/>)
             setLikeButton('inactive')
         }
         if (dislikeButtonActive === 'inactive'){
             setDislikeButton('active')
-            setDislike(<img src={require('../../.././assets/dislike-button-active.png')}/>)
+            setDislike(<img src={require('../../.././assets/dislike-button-active.png')} alt = ''/>)
         }
         else {
             setDislikeButton('inactive')
-            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')}/>)
+            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')} alt = ''/>)
         }
     }
     const [likeButtonActive, setLikeButton] = useState ('inactive')
     const [dislikeButtonActive, setDislikeButton] = useState ('inactive')
-    const [like, setLike] = useState(<img src={require('../../.././assets/like-button-inactive.png')}/>)
-    const [dislike, setDislike] = useState(<img src={require('../../.././assets/dislike-button-inactive.png')} />)
+    const [like, setLike] = useState(<img src={require('../../.././assets/like-button-inactive.png')} alt = ''/>)
+    const [dislike, setDislike] = useState(<img src={require('../../.././assets/dislike-button-inactive.png')} alt = ''/>)
 
     return ( 
         <div className='buttons'>
