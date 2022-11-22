fc788ac3a0cf9e831bcb33906e353f65b53431bb
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-11-21 19:49:55 -0800

    Added functionality to like and dislike buttons so only one can be active at once

--- src/Components/CreatePost/Buttons/Buttons.jsx
+++ src/Components/CreatePost/Buttons/Buttons.jsx
@@ -4,13 +4,45 @@ import './Buttons.css'
 
 
 const Buttons = (props) => {
+
+    function handleLikeClick(){
+        if (dislikeButtonActive === 'active'){
+            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')} />)
+            setDislikeButton('inactive')
+        }
+        if (likeButtonActive === 'inactive'){
+            setLikeButton('active')
+            setLike(<img src={require('../../.././assets/like-button-active.png')}/>)
+        }
+        else {
+            setLikeButton('inactive')
+            setLike(<img src={require('../../.././assets/like-button-inactive.png')}/>)
+        }
+    }
+
+    function handleDislikeClick(){
+        if (likeButtonActive === 'active'){
+            setLike(<img src={require('../../.././assets/like-button-inactive.png')} />)
+            setLikeButton('inactive')
+        }
+        if (dislikeButtonActive === 'inactive'){
+            setDislikeButton('active')
+            setDislike(<img src={require('../../.././assets/dislike-button-active.png')}/>)
+        }
+        else {
+            setDislikeButton('inactive')
+            setDislike(<img src={require('../../.././assets/dislike-button-inactive.png')}/>)
+        }
+    }
+    const [likeButtonActive, setLikeButton] = useState ('inactive')
+    const [dislikeButtonActive, setDislikeButton] = useState ('inactive')
     const [like, setLike] = useState(<img src={require('../../.././assets/like-button-inactive.png')}/>)
     const [dislike, setDislike] = useState(<img src={require('../../.././assets/dislike-button-inactive.png')} />)
 
     return ( 
         <div className='buttons'>
-            <div onClick={(event) => setLike(<img src={require('../../.././assets/like-button-active.png')}/>)}>{like}</div>
-            <div onClick={(event) => setDislike(<img src={require('../../.././assets/dislike-button-active.png')}/>)}>{dislike}</div>
+            <div onClick={handleLikeClick}>{like}</div>
+            <div onClick={handleDislikeClick}>{dislike}</div>
         </div>
      );
 }
